beea07b42df377d77511c255808feb1bcfb97de6
ClaireDL <cl.delleluche@gmail.com>
2020-10-26 23:01:49 +0000

    Worked on GemFinder

--- src/main/scala/com/clairedl/Algorithms.scala
+++ src/main/scala/com/clairedl/Algorithms.scala
@@ -31,7 +31,9 @@ class StringReducer(s: String) {
 class MissingInteger(array: Array[Int]) {
   def find(): Option[Int] = {
     val sorted = array.sorted
+
     var previous = sorted(0)
+
     var matches = new ListBuffer[Int]()
 
     for (number <- sorted) {
@@ -41,5 +43,40 @@ class MissingInteger(array: Array[Int]) {
 
     if (matches.length == 0) None
     else Some(matches.min)
+  }
+}
+
+/** We have a collection of rocks and we represent its minerals in a string with letters from [a-z] and we need to
+ * write a function to find the gemstones in the collection of rocks. A gemstone is a mineral that occurs at least once
+ * in each of the rocks.
+ * for ["abc","abaaa","bcd"] b is a gemstone since it appears in the three rocks
+ * for ["ab","cd","ef"] there's no gemstone */
+class GemstoneFinder(treasure: List[String]) {
+  def find(): Option[List[Char]] = {
+    // Gets how many times each unique mineral appears in a rock of the treasure
+    val inventory = treasure
+      .map(x => composition(x))
+      .flatten
+      .groupBy(x => x)
+      .map { case (k, v) => (k, v.length)}
+
+    var gems = new ListBuffer[Char]()
+
+    for (mineral <- inventory) {
+      if (mineral._2 == treasure.length) gems += mineral._1
+    }
+
+    gems.length match {
+      case 0 => None
+      case _ => Some(gems.toList)
+    }
+  }
+
+  // Gets the mineral composition of a rock
+  private def composition(rock: String): List[Char] = {
+    val result = rock
+      .groupBy(x => x)
+      .map(x => x._1)
+    result.toList
   }
 }
--- src/main/scala/com/clairedl/Main.scala
+++ src/main/scala/com/clairedl/Main.scala
@@ -14,4 +14,10 @@ object Main extends App {
 
   val missingInt3 = new MissingInteger(Array(1, 3, 5, 4, 1, 2)).find()
   println(missingInt3)
+
+  val gemstone = new GemstoneFinder(List("abca","abaaa","bcd")).find()
+  println(gemstone)
+
+  val gemstone2 = new GemstoneFinder(List("ab","cd","ef")).find()
+  println(gemstone2)
 }
